3ece95c13e10929bef195c7b90acf78b0b02a851
vancho-ks <kasumi_s@zigexn.co.jp>
2016-06-06 11:34:43 +0900

    SP版のデザイン着手

--- gulpfile.js
+++ gulpfile.js
@@ -3,46 +3,19 @@ var browserSync   = require('browser-sync');
 var runSequence   = require('run-sequence');
 var reload        = browserSync.reload;
 var sass          = require('gulp-ruby-sass');
-//var compass       = require('gulp-compass');
-//var autoprefixer  = require('gulp-autoprefixer');
-
-//var AUTOPREFIXER_BROWSERS = [
-//  'ie >= 8',
-//  'ie_mob >= 10',
-//  'ff >= 30',
-//  'chrome >= 34',
-//  'safari >= 7',
-//  'opera >= 23',
-//  'ios >= 7',
-//  'android >= 4.4',
-//  'bb >= 10'
-//];
 
 // Static server
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
-    	baseDir: "./docs/"
+      baseDir: "./"
     }
   });
 });
 
 // Compile sass, autoprefix
-//gulp.task('styles_pc', function() {
-//  return sass('stylesheets/theme/pc/master.scss', {
-//    bundleExec: true,
-//    style: 'expanded',
-//    compass: false
-//  })
-//  .on('error', function(err) {
-//    console.error('Error', err.message);
-//  })
-////  .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-//  .pipe(gulp.dest('dist/stylesheets/theme/pc'))
-//});
-
-gulp.task('styles_sp', function() {
-  return sass('stylesheets/theme/sp/master.scss', {
+gulp.task('sp_layout', function() {
+  return sass('stylesheets/', {
     bundleExec: true,
     style: 'expanded',
     compass: false
@@ -50,26 +23,12 @@ gulp.task('styles_sp', function() {
   .on('error', function(err) {
     console.error('Error', err.message);
   })
-//  .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-  .pipe(gulp.dest('docs/stylesheets/theme/sp'))
+  .pipe(gulp.dest('dist/stylesheets/'))
 });
-
-//gulp.task('common', function() {
-//  return sass('stylesheets/theme/common/master.scss', {
-//    bundleExec: true,
-//    style: 'expanded',
-//    compass: false
-//  })
-//  .on('error', function(err) {
-//    console.error('Error', err.message);
-//  })
-////  .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-//  .pipe(gulp.dest('dist/stylesheets/theme/common'))
-//});
 
 // Default task
 gulp.task('default', function() {
-  runSequence('styles_sp', 'browser-sync'),
+  runSequence('sp_layout', 'browser-sync'),
   gulp.watch(['*.html'], reload);
-  gulp.watch(['stylesheets/*.scss', 'stylesheets/**/*.scss'], ['styles_sp', reload]);
+  gulp.watch(['stylesheets/*.scss', 'stylesheets/**/*.scss'], ['sp_layout', reload]);
 });
